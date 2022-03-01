2da39552c74df46b6f91d707815ce3826ed9c042
luketeosmu <luke.teo.2020@sis.smu.edu.sg>
2022-03-01 19:59:11 +0800

    cart pushing

---
+++ chiak-app/src/components/Item.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <h3> {{ item.product }} </h3>
+        <img :src=item.image width="100" height="100">
+        <Button @click="clear(item.id)" text="Clear"> </Button>
+        <p>${{ item.price }} </p>
+        <div :key="item.id" v-for="nutrition in item.nutritionalInfo">
+           Energy: {{ nutrition.energy }} kcal
+        </div>
+        <div :key="item.id" v-for="nutrition in item.nutritionalInfo">
+           Protein: {{ nutrition.protein }} kcal
+        </div>
+        <div :key="item.id" v-for="nutrition in item.nutritionalInfo">
+           Total fats: {{ nutrition.fats }} kcal
+        </div>
+        <div :key="item.id" v-for="nutrition in item.nutritionalInfo">
+           Cholestrol: {{ nutrition.cholestrol }} kcal
+        </div>
+        <div :key="item.id" v-for="nutrition in item.nutritionalInfo">
+           Carbohydrate: {{ nutrition.carbohydrate }} kcal
+        </div>
+        <div :key="item.id" v-for="nutrition in item.nutritionalInfo">
+           Dietary Fibre: {{ nutrition.dietaryFibre }} kcal
+        </div>
+        <div :key="item.id" v-for="nutrition in item.nutritionalInfo">
+           Sodium: {{ nutrition.sodium }} kcal
+        </div>
+        <div :key="item.id" v-for="nutrition in item.nutritionalInfo">
+           Calcium: {{ nutrition.calcium }} kcal
+        </div>
+    </div>
+</template>
+
+<script>
+import Button from './Button.vue'
+export default {
+    name: 'Item',
+    props: {
+        item: Object
+    },
+    components: {
+        Button
+    },
+    methods: {
+        clear(id) {
+            this.$emit('clear-product', id)
+        }
+    }
+}
+</script>
---
+++ chiak-app/src/components/Items.vue
@@ -0,0 +1,23 @@
+<template>
+<div>
+    <div :key="item.id" v-for="item in items">
+        <Item @clear-product="$emit('clear-product', item.id)" :item="item" />
+    </div>
+    </div>
+</template>
+
+<script>
+import Item from './Item.vue'
+
+export default {
+    name: 'Items',
+    props: {
+        items: Array,
+    },
+    components: {
+        Item
+    },
+    emits: ['clear-product'],
+    
+}
+</script>
