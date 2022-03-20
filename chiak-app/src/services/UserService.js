453825e2b8baa5e12277869722e504322f4a2baa
ststheting <shaunts5111@gmail.com>
2022-03-20 20:51:33 +0800

    userservice

---
+++ chiak-app/src/services/UserService.js
@@ -0,0 +1,62 @@
+import axios from "axios";
+
+const REGISTER_API_URL = "http://localhost:8080/users";
+const AUTH_API_URL = "http://localhost:8080/users/search";
+
+class UserService {
+    registerUser = async (user) => {
+        return axios
+            .post(REGISTER_API_URL, user)
+            .then((response) => {
+                localStorage.clear();
+                // localStorage.setItem("tempPass", user.password);
+                // localStorage.setItem("user", JSON.stringify(response.data));
+                // localStorage.setItem("notLoggedIn", "")
+                // don't need to do above, above was created for the sake of RegisterBusiness
+
+                // console.log(localStorage.getItem("user"));
+                // return response.data;
+            })
+            .catch((err) => {
+                console.log(err);
+                // this.signOut();
+                return "failed";
+            });
+    };
+
+    authenticate = async (user) => {
+        return axios
+            .post(AUTH_API_URL, user)
+            .then((response) => {
+                localStorage.setItem("user", JSON.stringify(response.data));
+                console.log(localStorage.getItem("user"));
+                // return response.data;
+            })
+            .catch((err) => {
+                // console.log(err); //make this so 404 shows an alert
+                // throw err;
+                return "failed";
+            });
+    };
+
+    signOut() {
+        // localStorage.clear();
+        // localStorage.removeItem("notLoggedIn")
+        // localStorage.removeItem("tempPass")
+        localStorage.removeItem("user");
+    }
+
+    getCurrentUser() {
+        return JSON.parse(localStorage.getItem("user"));
+    }
+
+    isLoggedIn() {
+        // if (localStorage.getItem("user") === null || localStorage.getItem("notLoggedIn") == "") {
+        if (localStorage.getItem("user") === null) {
+            return false;
+        }
+        return true;
+    }
+
+}
+export default new UserService();
--- chiak-app/src/views/Login.vue
+++ chiak-app/src/views/Login.vue
@@ -14,38 +14,73 @@
         </div>
         <div class="flex w-1/2 justify-center items-center bg-white">
             <form class="bg-white">
-            <h1 class="text-gray-800 font-bold text-2xl mb-7">Hello Again!</h1>
-            <div class="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
-                </svg>
-                <input class="pl-2 outline-none border-none" type="email" name="" id="" placeholder="Email Address" />
-            </div>
-            <div class="flex items-center border-2 py-2 px-3 rounded-2xl">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
-                </svg>
-                <input class="pl-2 outline-none border-none" type="password" name="" id="" placeholder="Password" />
-            </div>
-            <button type="submit" class="block w-full bg-green-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2">Login</button>
-            <span @click='forgotPassword()' class="text-sm ml-2 hover:text-green-500 cursor-pointer">Forgot Password?</span><br><br>
-            <span @click='register()' class="text-sm ml-2 hover:text-green-500 cursor-pointer">Create an Account</span>
+                <h1 class="text-gray-800 font-bold text-2xl mb-7">Hello Again!</h1>
+                <div class="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
+                    </svg>
+                    <input v-model.lazy="input.username" class="pl-2 outline-none border-none" type="email" name="" placeholder="Email Address" />
+                </div>
+                <div class="flex items-center border-2 py-2 px-3 rounded-2xl">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clip-rule="evenodd" />
+                    </svg>
+                    <input v-model.lazy="input.password" class="pl-2 outline-none border-none" type="password" name="" placeholder="Password" />
+                </div>
+                <button @click='login()' type="button" class="block w-full bg-green-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2">Login</button>
+                <span @click='forgotPassword()' class="text-sm ml-2 hover:text-green-500 cursor-pointer">Forgot Password?</span><br><br>
+                <span @click='register()' class="text-sm ml-2 hover:text-green-500 cursor-pointer">Create an Account</span>
             </form>
         </div>
     </div>
 </template>
 <script>
+import UserService from '../services/UserService'
+
 export default {
     name: "Login",
+    components: {
+        UserService
+    },
     props: {
         
     },
     data () {
         return {
-            readMore: false
+            readMore: false,
+            input: {
+                username: "",
+                password: "",
+            }
         }
     },
     methods: {
+        login(){
+            try{
+                if(this.input.username != "" && this.input.password != "") {
+                    console.log(this.input);
+                    UserService.authenticate(input)
+                    .then((res) => {
+                        if(res == "failed"){
+                            console.log("Login failed")
+                        } else {
+                            this.$router.push('/')
+                        }
+                    })
+                    // if(localStorage.getItem("user") != null){
+                    //     this.$router.push('/');
+                    // } else {
+                    //     console.log("Login failed");
+                    // }
+                } else {
+                    console.log("A username and password must be present");
+                }
+            } catch (error){
+
+            }
+        },
+
+
         register(){
             this.$router.push('/register')
         },
